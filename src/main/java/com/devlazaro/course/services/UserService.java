3a370419eac2d40adb9af8ba2bff1d65df04556e
Lázaro Simão <lssimao.infor@gmail.com>
2020-12-12 18:12:55 -0300

    User insert

--- src/main/java/com/devlazaro/course/services/UserService.java
+++ src/main/java/com/devlazaro/course/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
 		Optional<User> obj = this.userRepository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return this.userRepository.save(obj);
+	}
 }
