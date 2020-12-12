afa6cd9c2874666921ed112bb0fcff5299e16dfe
Lázaro Simão <lssimao.infor@gmail.com>
2020-12-12 17:20:16 -0300

    OrderItem, many-to-many association with extra attributes - Parte 2

--- src/main/java/com/devlazaro/course/config/TestConfig.java
+++ src/main/java/com/devlazaro/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.devlazaro.course.entities.Category;
 import com.devlazaro.course.entities.Order;
+import com.devlazaro.course.entities.OrderItem;
 import com.devlazaro.course.entities.Product;
 import com.devlazaro.course.entities.User;
 import com.devlazaro.course.entities.enums.OrderStatus;
 import com.devlazaro.course.repositories.CategoryRepository;
+import com.devlazaro.course.repositories.OrderItemRepository;
 import com.devlazaro.course.repositories.OrderRepository;
 import com.devlazaro.course.repositories.ProductRepository;
 import com.devlazaro.course.repositories.UserRepository;
@@ -31,7 +33,9 @@ public class TestConfig implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
-
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
@@ -67,6 +71,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		this.productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		this.orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 }
--- src/main/java/com/devlazaro/course/entities/OrderItem.java
+++ src/main/java/com/devlazaro/course/entities/OrderItem.java
@@ -2,17 +2,20 @@ package com.devlazaro.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.devlazaro.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	private OrderItemPK id;
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -28,6 +31,7 @@ public class OrderItem implements Serializable {
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
