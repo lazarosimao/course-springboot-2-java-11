c6349623d1294648e575d292f9925dd0951f3938
Lázaro Simão <lssimao.infor@gmail.com>
2020-12-12 00:34:36 -0300

    OrderStatus enum

--- src/main/java/com/devlazaro/course/config/TestConfig.java
+++ src/main/java/com/devlazaro/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.devlazaro.course.entities.Order;
 import com.devlazaro.course.entities.User;
+import com.devlazaro.course.entities.enums.OrderStatus;
 import com.devlazaro.course.repositories.OrderRepository;
 import com.devlazaro.course.repositories.UserRepository;
 
@@ -30,9 +31,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		this.userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
 		this.orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
